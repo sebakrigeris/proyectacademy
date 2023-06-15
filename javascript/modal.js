dea607a968cf96619c7de2063f970978bee7e00d
Sebastian Krigeris <sebastiankri123@gmail.com>
2023-06-15 16:02:48 -0300

    repo3

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,23 +1,53 @@
-const spans = document.querySelectorAll('button[data-modal]');
-const modals = document.querySelectorAll('dialog');
 
-spans.forEach((span) => {
+document.addEventListener('DOMContentLoaded', () => {
+    const spans = document.querySelectorAll('button[data-modal]');
+    const modals = document.querySelectorAll('dialog');
+
+    spans.forEach((span) => {
     span.addEventListener('click', () => {
-    const modalId = span.dataset.modal;
-    const modal = document.getElementById(modalId);
-    modal.showModal();
-    modal.scrollTop = 0;
+        const modalId = span.dataset.modal;
+        const modal = document.getElementById(modalId);
+        modal.showModal();
 
-    modal.addEventListener('show', function() {
+        modal.addEventListener('show', () => {
+        modal.scrollTop = 0;
         modal.querySelector('h2').focus();
+        });
+    });
     });
 
-    });
-});
-
-modals.forEach((modal) => {
+    modals.forEach((modal) => {
     const closeButton = modal.querySelector('.btn-cerrar');
     closeButton.addEventListener('click', () => {
-    modal.close();
+        modal.close();
+    });
     });
 });
+
+
+
+
+
+// const spans = document.querySelectorAll('button[data-modal]');
+// const modals = document.querySelectorAll('dialog');
+
+// spans.forEach((span) => {
+//     span.addEventListener('click', () => {
+//     const modalId = span.dataset.modal;
+//     const modal = document.getElementById(modalId);
+//     modal.showModal();
+//     modal.scrollTop = 0;
+
+//     modal.addEventListener('show', function() {
+//         modal.querySelector('h2').focus();
+//     });
+
+//     });
+// });
+
+// modals.forEach((modal) => {
+//     const closeButton = modal.querySelector('.btn-cerrar');
+//     closeButton.addEventListener('click', () => {
+//     modal.close();
+//     });
+// });
