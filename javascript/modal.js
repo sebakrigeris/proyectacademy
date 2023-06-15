2229098780db61d64e35e4f31a26c9e9180456ee
Sebastian Krigeris <sebastiankri123@gmail.com>
2023-06-15 16:17:49 -0300

    rep4

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,53 +1,23 @@
+const spans = document.querySelectorAll('button[data-modal]');
+const modals = document.querySelectorAll('dialog');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const spans = document.querySelectorAll('button[data-modal]');
-    const modals = document.querySelectorAll('dialog');
-
-    spans.forEach((span) => {
+spans.forEach((span) => {
     span.addEventListener('click', () => {
-        const modalId = span.dataset.modal;
-        const modal = document.getElementById(modalId);
-        modal.showModal();
+    const modalId = span.dataset.modal;
+    const modal = document.getElementById(modalId);
+    modal.showModal();
+    modal.scrollTop = 0;
 
-        modal.addEventListener('show', () => {
-        modal.scrollTop = 0;
+    modal.addEventListener('show', function() {
         modal.querySelector('h2').focus();
-        });
-    });
     });
 
-    modals.forEach((modal) => {
-    const closeButton = modal.querySelector('.btn-cerrar');
-    closeButton.addEventListener('click', () => {
-        modal.close();
-    });
     });
 });
 
-
-
-
-
-// const spans = document.querySelectorAll('button[data-modal]');
-// const modals = document.querySelectorAll('dialog');
-
-// spans.forEach((span) => {
-//     span.addEventListener('click', () => {
-//     const modalId = span.dataset.modal;
-//     const modal = document.getElementById(modalId);
-//     modal.showModal();
-//     modal.scrollTop = 0;
-
-//     modal.addEventListener('show', function() {
-//         modal.querySelector('h2').focus();
-//     });
-
-//     });
-// });
-
-// modals.forEach((modal) => {
-//     const closeButton = modal.querySelector('.btn-cerrar');
-//     closeButton.addEventListener('click', () => {
-//     modal.close();
-//     });
-// });
+modals.forEach((modal) => {
+    const closeButton = modal.querySelector('.btn-cerrar');
+    closeButton.addEventListener('click', () => {
+    modal.close();
+    });
+});
